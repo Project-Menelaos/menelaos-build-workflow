5d96ff1c261fe36161671a79952274f5ffaa693e
James Swineson <jamesswineson@gmail.com>
2016-01-12 23:44:02 +0800

    fix: too much graph

--- gulp/llvm-ir.js
+++ gulp/llvm-ir.js
@@ -77,25 +77,30 @@ var savefile = function(filename, string) {
   fs.writeFileSync(filename, string);
 };
 
+getFiles = function(dir) {
+  return fs.readdirSync(dir).filter(function(file) {
+    return fs.statSync(path.join(dir, file)).isFile();
+  });
+};
+
 gulp.task('compile-graph', function() {
   scriptsPath = './build/dot-src';
   var folders = getFolders(scriptsPath);
-  folders.map(function(folder) {
+  folders.forEach(function(folder) {
     mdString = "";
-    walk.walk(path.join(scriptsPath, folder))
-        .on("file",
-            function(root, fileStat, next) {
-              filename = fileStat.name;
-              console.log(filename);
-              if ([ '.png' ].contains(path.extname(filename))) {
-                filepath = path.resolve(root, filename);
-                title = filename.replace(".dot.png", '');
-                mdString += mkimage(filepath, title);
-              }
-              next();
-            })
-        .on('end', function() {
-          savefile(path.join(scriptsPath, folder + ".md"), mdString);
-        });
+    console.log(path.join(scriptsPath, folder));
+    files = getFiles(path.join(scriptsPath, folder));
+    for (var i in files) {
+      filename = files[i];
+      if ([ '.png' ].contains(path.extname(filename))) {
+        console.log(filename);
+        filepath = path.resolve(path.join(scriptsPath, folder), filename);
+        title = filename.replace(".dot.png", '');
+        mdString += mkimage(filepath, title);
+      }
+    }
+
+    savefile(path.join(scriptsPath, folder + ".md"), mdString);
+
   });
 });
